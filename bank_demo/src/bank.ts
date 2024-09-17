1e724742b40052d039e5896045d3929959852a97
nsp5488 <nsp5488@g.rit.edu>
2024-09-17 04:20:14 -0400

    implemented checking balance of accounts, added a private helper for getting accounts by number. resolves #4

--- bank_demo/src/bank.ts
+++ bank_demo/src/bank.ts
@@ -12,7 +12,14 @@ interface BankAccount {
 export default class Bank {
     // Hashmap from account number to BankAccount to store new accounts in.
     private accounts : Map<string, BankAccount> = new Map();
-
+    
+    private getAccountOrError(accountNumber:string): BankAccount {
+        let account = this.accounts.get(accountNumber);
+        if (!account) {
+            throw new Error("Account does not exist!");
+        }
+        return account;
+    }
     /**
      * Creates a new account if the information entered is valid (no duplicate account numbers).
      * @param {string} name The name of the user creating the account
@@ -35,4 +42,42 @@ export default class Bank {
             return account
         }
     }
+    /**
+     * Deposits money into an account if possible.
+     * @param {string} accountNumber The account number to deposit into.
+     * @param {number} amount The amount to deposit (must be positive).
+     * @returns {number} The new balance of the account.
+     */
+    public deposit(accountNumber: string, amount: number): number {
+        if(amount <= 0) {
+            throw new Error("Invalid deposit amount!");
+        }
+        let account = this.getAccountOrError(accountNumber)
+        account.balance += amount;
+        return account.balance;
+    }
+
+    /**
+     * Withdraws money from an account if possible.
+     * @param accountNumber The account to withdraw from.
+     * @param amount The amount to withdraw.
+     * @returns {number} The new balance of the account.
+     */
+    public withdraw(accountNumber: string, amount:number): number {
+        if(amount <= 0) {
+            throw new Error("Invalid withdrawl amount");
+        }
+        let account = this.getAccountOrError(accountNumber)
+        account.balance -= amount;
+        return account.balance;
+    }
+
+    /**
+     * Returns the balance of a given account.
+     * @param accountNumber The account to check the balance of.
+     * @returns The balance of the provided account, if it exists.
+     */
+    public checkBalance(accountNumber: string): number {
+        return this.getAccountOrError(accountNumber).balance;
+     }
 }
