1dc3e02261ef11754546c9a0a7de60c5dad73c54
nsp5488 <nsp5488@g.rit.edu>
2024-09-17 04:08:38 -0400

    resolves #2

--- bank_demo/test/bankTest.ts
+++ bank_demo/test/bankTest.ts
@@ -2,6 +2,7 @@ import Bank from "../src/bank";
 
 const bank = new Bank();
 
+console.log("Testing account creation")
 const account = bank.createAccount("Test Name", 22, "12341234");
 if (account.accountNumber == "12341234") {
     console.log("Scenario 1 passed");
@@ -14,5 +15,31 @@ try {
     console.log("Scenario 2 failed");
 } catch(_) {
     console.log("Scenario 2 passed");
-
 }
+
+console.log("Account creation tests complete.");
+
+console.log("Testing deposit");
+
+bank.deposit("12341234", 50);
+if (account.balance === 50) {
+    console.log("Scenario 3 passed")
+} else {
+    console.log("Scenario 3 failed");
+}
+try {
+    bank.deposit("12341234", -1)
+    console.log("Scenario 4 failed");
+} catch(_) {
+    console.log("Scenario 4 passed")
+}
+
+try {
+    bank.deposit("abc", 50)
+    console.log("Scenario 4 failed");
+} catch(_) {
+    console.log("Scenario 4 passed")
+}
+
+console.log("deposit tests complete.");
+
